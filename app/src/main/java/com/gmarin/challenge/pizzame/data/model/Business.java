d694ccbaa3694843fab663f2054283c0cec40bec
gmarin <gus.marin@sea.samsung.com>
2019-03-06 15:52:03 -0500

    Base Business Model
    - Missing image and review count TBD
    
    Change-Id: If75532005d6b18f50a5dab3506f7a0062513edad

---
+++ app/src/main/java/com/gmarin/challenge/pizzame/data/model/Business.java
@@ -0,0 +1,87 @@
+package com.gmarin.challenge.pizzame.data.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Business {
+    @SerializedName("id")
+    private String id;
+
+    @SerializedName("name")
+    private String name;
+
+    @SerializedName("rating")
+    private double rating;
+
+    @SerializedName("display_phone")
+    private String display_phone;
+
+    @SerializedName("distance")
+    private double distance;
+
+    @SerializedName("location")
+    private Location location;
+
+    public static class Location {
+        @SerializedName("display_address")
+        private List<String> display_address;
+
+        public List<String> getDisplay_address() {
+            return display_address;
+        }
+
+        public void setDisplay_address(List<String> display_address) {
+            this.display_address = display_address;
+        }
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getDisplay_phone() {
+        return display_phone;
+    }
+
+    public void setDisplay_phone(String display_phone) {
+        this.display_phone = display_phone;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+}
